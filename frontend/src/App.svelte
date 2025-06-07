087a06f1447958db281fe73d40779011910acf1c
Maximilien Valenzano <slordef.vikamet@gmail.com>
2025-06-07 23:33:05 +0200

    chore: add sections

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+import Info from "./Info.svelte";
+import ContactUs from "./ContactUs.svelte";
 </script>
 
 <main>
   <div class="logo">
     <img src="/logo.svg" alt="Logo" />
+  </div>
+  <div class="content">
+    <Info />
+    <ContactUs />
   </div>
 </main>
 
---
+++ frontend/src/Info.svelte
@@ -0,0 +1,6 @@
+<section>
+    <h1>About</h1>
+    <p>
+        Nebula Silva is black metal band from Switzerland.
+    </p>
+</section>
