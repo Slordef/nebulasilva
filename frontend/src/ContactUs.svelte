48eb1bc683fc140edc204df27c4c644ad248341c
Maximilien Valenzano <slordef.vikamet@gmail.com>
2025-06-08 23:07:17 +0200

    chore: add informations

--- frontend/src/ContactUs.svelte
+++ frontend/src/ContactUs.svelte
@@ -1,6 +1,6 @@
 <section>
     <h2>Contact Us</h2>
     <p>
-        Contact us at <a href="mailto:info@nebulasilva.com">info@nebulasilva.com</a> for gigs.
+        Contact us at <a href="mailto:info@nebulasilva.com">info@nebulasilva.com</a>.
     </p>
 </section>
